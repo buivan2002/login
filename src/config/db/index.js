e4b5d71e674109ed65cedb93d9c455ec1b5637f0
buivan2002 <buiquocanh1612@gmail.com>
2024-08-24 23:17:11 +0700

    add

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose')
 
 async function connect(){
     try{
-        await mongoose.connect('mongodb://localhost:27017/app_dev',{
-            useNewUrlParser : true,
-            useUnifiedTopology: true
+        await mongoose.connect('mongodb+srv://vercel-admin-user:mbYFfQ8SlGTF0poG@cluster0.hc9av.mongodb.net/app_dev?retryWrites=true&w=majority&appName=Cluster0',{
+            // useNewUrlParser : true,
+            // useUnifiedTopology: true
         })
         console.log('ok')
     }
